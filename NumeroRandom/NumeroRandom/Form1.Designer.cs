cee133d716e7c5c0a736aa1e46469836db163d91
AlejandroGomezV <m.alejandro.gomez.v@gmail.com>
2022-01-10 20:25:08 -0600

    Primer avance
    Generacion proyecto
    Inicializar matriz de numeros
    Generar numero random
    Agregar numeros manualmente

---
+++ NumeroRandom/NumeroRandom/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace NumeroRandom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listGenerados = new System.Windows.Forms.ListView();
+            this.listBloqueados = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnrandom = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnbloquear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 707);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // listGenerados
+            // 
+            this.listGenerados.HideSelection = false;
+            this.listGenerados.Location = new System.Drawing.Point(823, 90);
+            this.listGenerados.Name = "listGenerados";
+            this.listGenerados.Size = new System.Drawing.Size(121, 629);
+            this.listGenerados.TabIndex = 1;
+            this.listGenerados.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBloqueados
+            // 
+            this.listBloqueados.HideSelection = false;
+            this.listBloqueados.Location = new System.Drawing.Point(976, 108);
+            this.listBloqueados.Name = "listBloqueados";
+            this.listBloqueados.Size = new System.Drawing.Size(121, 611);
+            this.listBloqueados.TabIndex = 2;
+            this.listBloqueados.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(823, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numeros generados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(976, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numeros bloqueados";
+            // 
+            // btnrandom
+            // 
+            this.btnrandom.Location = new System.Drawing.Point(823, 32);
+            this.btnrandom.Name = "btnrandom";
+            this.btnrandom.Size = new System.Drawing.Size(121, 23);
+            this.btnrandom.TabIndex = 5;
+            this.btnrandom.Text = "Generar nuevo";
+            this.btnrandom.UseVisualStyleBackColor = true;
+            this.btnrandom.Click += new System.EventHandler(this.btnrandom_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(976, 61);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(65, 23);
+            this.txtNum.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(976, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bloquear numero";
+            // 
+            // btnbloquear
+            // 
+            this.btnbloquear.Location = new System.Drawing.Point(1057, 61);
+            this.btnbloquear.Name = "btnbloquear";
+            this.btnbloquear.Size = new System.Drawing.Size(40, 23);
+            this.btnbloquear.TabIndex = 8;
+            this.btnbloquear.Text = "+";
+            this.btnbloquear.UseVisualStyleBackColor = true;
+            this.btnbloquear.Click += new System.EventHandler(this.btnbloquear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 731);
+            this.Controls.Add(this.btnbloquear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnrandom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBloqueados);
+            this.Controls.Add(this.listGenerados);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListView listGenerados;
+        private System.Windows.Forms.ListView listBloqueados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnrandom;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnbloquear;
+    }
+}
+
---
+++ NumeroRandom/NumeroRandom/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroRandom
+{
+    public partial class Form1 : Form
+    {
+        private List<int> listanumerosbloquedos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listanumerosbloquedos = new List<int>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 65; i < 80; i++)
+            {
+                DataGridViewTextBoxColumn columna = new DataGridViewTextBoxColumn();
+                columna.HeaderText = Convert.ToChar(i).ToString();
+                columna.Width = 50;
+                columna.ReadOnly = true;
+
+                dataGridView1.Columns.Add(columna);
+            }
+
+            int cont = 1;
+            for (int i = 1; i < 26; i++)
+            {
+                int index = dataGridView1.Rows.Add();
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (cont <= 365)
+                    {
+                        dataGridView1.Rows[index].Cells[j].Value = cont;
+                        cont++;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[index].Cells[j].Value = 0;
+                    }
+                }
+            }
+        }
+
+        private void btnrandom_Click(object sender, EventArgs e)
+        {
+            generarRandom();
+        }
+
+        private void generarRandom()
+        {
+            Random myObject = new Random();
+            int ranNum = myObject.Next(1, 365);
+
+            if (listanumerosbloquedos.Count > 0)
+            {
+                while (listanumerosbloquedos.Contains(ranNum))
+                {
+                    ranNum = myObject.Next(1, 365);
+                }
+            }
+
+            var result = MessageBox.Show("Numero generado: " + ranNum + "\nSi: Aceptar\nNo: Generar Nuevo", "Numero generado",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+                        {
+                            if (dataGridView1.Rows[i].Cells[j].Value.ToString() == ranNum.ToString())
+                            {
+                                dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Green;
+                                listanumerosbloquedos.Add(ranNum);
+                                listGenerados.Items.Add(ranNum.ToString());
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (result == DialogResult.No)
+            {
+                generarRandom();
+            }
+        }
+
+        private void btnbloquear_Click(object sender, EventArgs e)
+        {
+            int numerobloqueado = int.Parse(txtNum.Text);
+
+            if (listanumerosbloquedos.Contains(numerobloqueado))
+            {
+                MessageBox.Show("Ingrese otro numero", "Numero repetido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNum.Text = "";
+            }
+            else
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Rows[i].Cells.Count; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value.ToString() == numerobloqueado.ToString())
+                        {
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Orange;
+                            listanumerosbloquedos.Add(numerobloqueado);
+                            listBloqueados.Items.Add(numerobloqueado.ToString());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
